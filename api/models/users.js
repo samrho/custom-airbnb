85386fb29de71b3eeee91b1b3292cbe1e49ff354
Sam Rho <seungnam2@gmail.com>
2019-10-15 20:46:25 +0900

    feat: sequelize-cli

---
+++ api/models/users.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+	let user = sequelize.define(
+		"User",
+		{
+			userID: {
+				field: "user_id",
+				type: DataTypes.STRING(50),
+				unique: true,
+				allowNull: false,
+			},
+			password: {
+				field: "password",
+				type: DataTypes.STRING(30),
+				allowNull: false,
+			},
+		},
+		{
+			underscored: true,
+			freezeTableName: true,
+			tableName: "user",
+		},
+	);
+	return user;
+};
